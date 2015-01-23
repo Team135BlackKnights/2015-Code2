be85a59f4409621916d2155546715f899d3e7b90
team135 <robotics@phm.k12.in.us>
2015-01-23 14:02:34 -0500

    ChangeDriveMode added

---
+++ UnderstandingCommandBasedProgramming/src/Commands/ChangeDriveMode.cpp
@@ -0,0 +1,40 @@
+#include "ChangeDriveMode.h"
+
+ChangeDriveMode::ChangeDriveMode(int mode)
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+	Requires(mecanumDrive);
+	driveMode = mode;
+}
+
+// Called just before this Command runs the first time
+void ChangeDriveMode::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ChangeDriveMode::Execute()
+{
+	mecanumDrive->SetDriveMode(driveMode);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool ChangeDriveMode::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void ChangeDriveMode::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ChangeDriveMode::Interrupted()
+{
+
+}
