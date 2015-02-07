0e2a0d570245a2ffb826030fed71bc8529b17f9d
team135 <robotics@phm.k12.in.us>
2015-02-07 14:33:50 -0500

    Has button box for external manipulator and other stuff.

--- CommandBasedv5/src/OI.cpp
+++ CommandBasedv5/src/OI.cpp
@@ -18,34 +18,29 @@ OI::OI()
 	// Process operator interface input here.
 	sticks[LEFT] = new Joystick(JOYSTICK_LEFT); //Declares new Joysticks
 	sticks[RIGHT] = new Joystick(JOYSTICK_RIGHT);
-	//sticks[BUTTON] = new Joystick(JOYSTICK_BUTTON);
+	sticks[BUTTON] = new Joystick(JOYSTICK_BUTTON);
 
-	for (int i = 0; i < 2; i++) //Declares new buttons for Joysticks max # of buttons is 12
+	for (int i = 0; i < 3; i++) //Declares new buttons for Joysticks max # of buttons is 12
 	{
 		for (int j = 1; j <= MAX_JOYSTICK_BUTTONS; j++)
 		{
 			buttons[i][j] = new JoystickButton(sticks[i], j);
 		}
 	}
-	//buttons[LEFT][10]->WhenPressed(new AlignRobot(45)); // When pressed Robot Aligns to 45 degrees
-	//buttons[LEFT][BUTTON_OPEN_CLOSE_CLAW]->WhenPressed(new ExternalOpenCloseClaw(ExternalCollect::CLAW_OPEN));
-	//buttons[BUTTON][BUTTON_OPEN_CLOSE_CLAW]->WhenReleased(new ExternalOpenCloseClaw(ExternalCollect::CLAW_CLOSED));
-	//buttons[LEFT][10]->WhenPressed(new ExternalHingeClaw(ExternalCollect::CLAW_UP));
-	//buttons[LEFT][10]->WhenReleased(new ExternalHingeClaw(ExternalCollect::CLAW_DOWN));
-	buttons[LEFT][11]->WhenPressed(new ExternalHingeClaw(false));
-	buttons[LEFT][12]->WhenReleased(new ExternalHingeClaw(true));
+	//checked
+	buttons[BUTTON][BUTTON_OPEN_CLOSE_CLAW]->WhenPressed(new ExternalOpenCloseClaw(ExternalCollect::CLAW_OPEN));
+	buttons[BUTTON][BUTTON_OPEN_CLOSE_CLAW]->WhenReleased(new ExternalOpenCloseClaw(ExternalCollect::CLAW_CLOSED));
+
+	//checked
+	buttons[BUTTON][BUTTON_HINGE_CLAW]->WhenPressed(new ExternalHingeClaw(ExternalCollect::CLAW_UP));
+	buttons[BUTTON][BUTTON_HINGE_CLAW]->WhenReleased(new ExternalHingeClaw(ExternalCollect::CLAW_DOWN));
+
+	//checked
+	buttons[BUTTON][BUTTON_LEFTY_FLIP]->WhenPressed(new LeftyModeJustForRiley());
 
 
-	buttons[LEFT][7]->WhenPressed(new ExternalOpenCloseClaw(false));
-	buttons[LEFT][8]->WhenReleased(new ExternalOpenCloseClaw(true));
+	//buttons[BUTTON][BUTTON_RUN_WINCH_UNTIL_STOPPED]->WhenPressed(new RunWinchUntilStopped(-1, 0));
 
-	buttons[LEFT][3]->WhenPressed(new LeftyModeJustForRiley());
-
-	//buttons[LEFT][9]->WhenPressed(new RunWinchUntilStopped(.5, externalCollect->GetUpperSwitchValue()))
-	buttons[LEFT][9]->WhenPressed(new RunWinchUntilStopped(-1, 0));
-
-	//buttons[RIGHT][10]->WhenPressed(new ExternalOpenCloseClaw(false));
-	//buttons[RIGHT][10]->WhenReleased(new ExternalOpenCloseClaw(true));
 	//buttons[BUTTON][BUTTON_LEFTY_FLIP]->WhenPressed(new LeftyModeJustForRiley());
 	SmartDashboard::PutBoolean(T_LEFTY_MODE, flipped);
 
@@ -64,6 +59,11 @@ float OI::GetStickY(int hand)
 float OI::GetStickTwist(int hand)
 {
 	return sticks[hand]->GetTwist(); // Returns Twist or Z Axis of Joysticks
+}
+
+bool OI::GetButton(int stick, int button)
+{
+	return buttons[stick][button]->Get();
 }
 
 void OI::LeftyFlip()
--- CommandBasedv5/src/OI.h
+++ CommandBasedv5/src/OI.h
@@ -6,9 +6,9 @@
 class OI
 {
 public:
-	static const int MAX_JOYSTICK_BUTTONS = 12;
-	Joystick* sticks[2]; //0 is Left, 1 is Right
-	JoystickButton* buttons[2][MAX_JOYSTICK_BUTTONS + 1];
+	static const int MAX_JOYSTICK_BUTTONS = 20;
+	Joystick* sticks[3]; //0 is Left, 1 is Right
+	JoystickButton* buttons[3][MAX_JOYSTICK_BUTTONS + 1];
 
 public:
 	int LEFT = 0;
@@ -17,13 +17,15 @@ public:
 	bool flipped = false;
 
 	static const int BUTTON_OPEN_CLOSE_CLAW = 15;
-	static const int BUTTON_HINGE_CLAW = 14;
+	static const int BUTTON_HINGE_CLAW = 13;
 	static const int BUTTON_LEFTY_FLIP = 12;
+	static const int BUTTON_RUN_WINCH_UNTIL_STOPPED = 9;
 
 	OI();
 	float GetStickX(int);
 	float GetStickY(int);
 	float GetStickTwist(int);
+	bool GetButton(int stick, int);
 	void LeftyFlip();
 };
 
